ebdb5ac0fbce8f649e05f7291c9ff5ac208a995e
Aditya <2005akjha@gmail.com>
2025-05-24 13:18:36 +0530

    updated navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
-
+// import { Navbar } from '@/components/ui/Navbar'
 export const metadata: Metadata = {
   title: 'v0 App',
   description: 'Created with v0',
@@ -14,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {/* <Navbar /> */}
+        {children}
+      </body>
     </html>
   )
 }
